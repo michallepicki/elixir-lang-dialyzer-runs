d0dc730eecba051f8978ed1c40ffd90cfed68d3d
Michał Łępicki <michal.lepicki@gmail.com>
2020-06-23 09:00:33 +0200

    updates!
    closes #2
    closes #5

--- dialyze.exs
+++ dialyze.exs
@@ -7,7 +7,7 @@ defmodule Dialyzin do
     filter(rest, [filter_warning(warning) | acc])
   end
 
-  defp filter_warning(warning = {:warn_failing_call, {'lib/logger.ex', line}, {:call, [:logger, :macro_log, [?(, ?\#, ?{, ?}, ?, | _], [1], :only_contract, _, _, _]}}) when line in [905, 896],
+  defp filter_warning(warning = {:warn_failing_call, {'lib/logger.ex', line}, {:call, [:logger, :macro_log, [?(, ?\#, ?{, ?}, ?, | _], [1], :only_contract, _, _, _]}}) when line in [896, 903],
     do: {:ok, "Elixir deliberately using erlang macro-based logger interface without passing in call location", warning}
 
   defp filter_warning(
@@ -22,7 +22,7 @@ defmodule Dialyzin do
        ),
        do: {:ok, "Elixir folks want to be able to pattern match on a struct name while keeping the struct type opaque", warning}
 
-  defp filter_warning(warning = {:warn_matching, {'lib/kernel.ex', line}, {:pattern_match, ['pattern \'false\'', '\'true\'']}}) when line in [1798, 3001, 3325, 3630, 3975, 3979, 4416],
+  defp filter_warning(warning = {:warn_matching, {'lib/kernel.ex', line}, {:pattern_match, ['pattern \'false\'', '\'true\'']}}) when line in [1804, 3007, 3331, 3636, 3981, 3985, 4422],
     do: {:ok, "inlined bootstrap check stuff", warning}
 
   defp filter_warning(warning = {:warn_matching, {'src/elixir_erl_compiler.erl', 59}, {:pattern_match, _lots_of_details}}),
@@ -43,7 +43,16 @@ defmodule Dialyzin do
   defp filter_warning(warning = {:warn_return_no_exit, {'lib/iex.ex', 652}, {:no_return, [:only_normal, :__break__!, 2]}}),
     do: {:ok, "not annotated exception", warning}
 
+  defp filter_warning(warning = {:warn_return_no_exit, {'src/elixir_quote.erl', 420}, {:no_return, [:only_normal, :bad_escape, 1]}}),
+    do: {:ok, "not annotated exception", warning}
+
+  defp filter_warning(warning = {:warn_return_no_exit, {'lib/mix/tasks/iex.ex', 9}, {:no_return, [:only_normal, :run, 1]}}),
+    do: {:ok, "not annotated exception", warning}
+
   defp filter_warning(warning = {:warn_return_no_exit, {'lib/iex/cli.ex', 151}, {:no_return, [:only_normal]}}),
+    do: {:ok, "not annotated exit", warning}
+
+  defp filter_warning(warning = {:warn_return_no_exit, {'lib/gen_event.ex', 460}, {:no_return, [:only_normal, :system_terminate, 4]}}),
     do: {:ok, "not annotated exit", warning}
 
   defp filter_warning(warning = {:warn_return_no_exit, {'lib/elixir/src/elixir_parser.yrl', _}, {:no_return, [:only_normal, function, _arity]}})
@@ -115,10 +124,28 @@ defmodule Dialyzin do
   defp filter_warning(warning = {:warn_not_called, {'lib/base.ex', _}, {:unused_fun, [function, _]}}) when function in [:encode_pair_clauses, :shift, :encode_clauses, :decode_char_clauses, :decode_mixed_clauses, :decode_clauses, :bad_digit_clause],
     do: {:ok, "functions inlined or only used to generate other functions at compile time", warning}
 
+  defp filter_warning(warning = {:warn_not_called, {'lib/system.ex', 91}, {:unused_fun, [function, 1]}}) when function in [:read_stripped, :strip],
+    do: {:ok, "functions called only during elixir compilation time", warning}
+
+  defp filter_warning(warning = {:warn_matching, {'lib/dynamic_supervisor.ex', 350}, {:pattern_match_cov, ['variable _other@1', '{_,_,_,_,_,_} | \#{\'id\':=_, \'start\':={atom(),atom(),[any()]}, \'modules\'=>\'dynamic\' | [atom()], \'restart\'=>\'permanent\' | \'temporary\' | \'transient\', \'shutdown\'=>\'brutal_kill\' | \'infinity\' | non_neg_integer(), \'type\'=>\'supervisor\' | \'worker\'}']}}),
+    do: {:ok, "overly defensive code", warning}
+
+  defp filter_warning(warning = {:warn_matching, {'lib/string_io.ex', 410}, {:guard_fail, [:is_list, '(_data@1::binary())']}}),
+    do: {:ok, "overly defensive code", warning}
+
+  defp filter_warning(warning = {:warn_matching, {'lib/mix/utils.ex', 672}, {:pattern_match, ['pattern \'nil\'', '\#{\'__struct__\':=\'Elixir.URI\', \'authority\':=\'nil\' | binary(), \'fragment\':=\'nil\' | binary(), \'host\':=\'nil\' | binary(), \'path\':=\'nil\' | binary(), \'port\':=\'nil\' | char(), \'query\':=\'nil\' | binary(), \'scheme\':=\'nil\' | binary(), \'userinfo\':=\'nil\' | binary()}']}}),
+    do: {:ok, "overly defensive code", warning}
+
+  defp filter_warning(warning = {:warn_matching, {'lib/mix/tasks/deps.compile.ex', 236}, {:guard_fail, ['_@6::\'true\'', '=:=', '\'nil\'']}}),
+    do: {:ok, "slightly dead code", warning}
+
+  defp filter_warning(warning = {:warn_matching, {'lib/calendar/time.ex', 642}, {:pattern_match, ['pattern {\'error\', _reason@1}', '{\'ok\',\#{\'__struct__\':=\'Elixir.Time\', \'calendar\':=atom(), \'hour\':=non_neg_integer(), \'microsecond\':={non_neg_integer(),non_neg_integer()}, \'minute\':=non_neg_integer(), \'second\':=non_neg_integer()}}']}}),
+    do: {:ok, "slightly dead code", warning}
+
   defp filter_warning(warning), do: {:warning, warning}
 end
 
-warnings =
+dialyzer_output =
   :dialyzer.run(
     init_plt: 'plt-dir/.elixir.plt',
     native: true,
@@ -135,8 +162,11 @@ warnings =
     ]
   )
 
-warnings
-|> Dialyzin.filter([])
+results = Dialyzin.filter(dialyzer_output, [])
+
+has_potential_issues? = Enum.any?(results, fn tup -> elem(tup, 0) == :warning end)
+
+results
 |> Enum.sort_by(fn
   {:warning, _} -> 1
   {:ok, _, _} -> 2
@@ -148,14 +178,17 @@ end)
       |> :dialyzer.format_warning(indent_opt: true, filename_opt: :fullpath)
       |> to_string()
 
-    # "Potential issue: #{formatted_warning}\n\n" <> "raw form: #{inspect(warning)}\n\n"
-    formatted_warning <> "\n"
+    "Potential issue: #{formatted_warning}\n\n" <> "raw form: #{inspect(warning)}\n\n"
 
-  {:ok, _message, _warning} ->
-    # "...something we can ignore (#{message})\n\n" <> "raw form: #{inspect(warning)}\n\n"
-    nil
+  {:ok, message, warning} ->
+    "Filtered non-issue: (#{message})\n\n" <> "raw form: #{inspect(warning)}\n\n"
 end)
-|> Enum.filter(& &1)
 |> Stream.into(File.stream!("report", [:write, :utf8]))
 |> Stream.into(IO.stream(:stdio, :line))
 |> Stream.run()
+
+if(has_potential_issues?) do
+  System.halt(1)
+else
+  nil
+end
