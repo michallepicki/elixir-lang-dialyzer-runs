c09b7d65cc3aa712432ffa67ee0a2c5d3d0f2136
Michał Łępicki <michal.lepicki@gmail.com>
2020-12-26 13:28:43 +0100

    records

--- dialyze.exs
+++ dialyze.exs
@@ -1,23 +1,25 @@
 defmodule Dialyze do
-  expected = %{}
+  require Record
 
-  def filter(warnings) do
-    filter(warnings, [])
-  end
+  Record.defrecord(:filtered, id: nil, comment: nil, data: nil)
+  Record.defrecord(:unfiltered, data: nil)
+  Record.defrecord(:unexpected_count, id: nil, actual: nil, expected: nil)
 
-  defp filter([], acc) do
-    Enum.reverse(acc)
-  end
+  expected_counts = %{}
 
-  defp filter([warning | rest], acc) do
-    filter(rest, [filter_warning(warning) | acc])
-  end
+  defp filter(warnings), do: filter(warnings, [])
 
+  defp filter([], acc), do: Enum.reverse(acc)
+
+  defp filter([warning | rest], acc), do: filter(rest, [filter_warning(warning) | acc])
+
+  # 1
   defp filter_warning(warning = {:warn_failing_call, {'lib/logger.ex', 770}, {:call, [Logger, :__do_log__, _, [3], :only_sig, _, _, {false, :none}]}}),
-    do: {:ok, "Elixir deliberately using erlang macro-based logger interface without passing in call location", warning, 1}
+    do: filtered(id: 1, comment: "Elixir deliberately using erlang macro-based logger interface without passing in call location", data: warning)
 
-  expected = Map.put(expected, 1, 1)
+  expected_counts = Map.put(expected_counts, 1, 1)
 
+  # 2
   defp filter_warning(
          warning =
            {:warn_opaque, {'lib/mix/tasks/test.ex', 613},
@@ -28,70 +30,83 @@ defmodule Dialyze do
                '\'Elixir.MapSet\':t(binary() | maybe_improper_list(binary() | maybe_improper_list(any(),binary() | []) | char(),binary() | []))'
              ]}}
        ),
-       do: {:ok, "Elixir folks want to be able to pattern match on a struct name while keeping the struct type opaque", warning, 2}
+       do: filtered(id: 2, comment: "Elixir folks want to be able to pattern match on a struct name while keeping the struct type opaque", data: warning)
 
-  expected = Map.put(expected, 2, 1)
+  expected_counts = Map.put(expected_counts, 2, 1)
 
+  # 3
   defp filter_warning(warning = {:warn_matching, {'lib/kernel.ex', _}, {:pattern_match, ['pattern \'false\'', '\'true\'']}}),
-    do: {:ok, "inlined bootstrap check stuff", warning, 3}
+    do: filtered(id: 3, comment: "inlined bootstrap check stuff", data: warning)
 
-  expected = Map.put(expected, 3, 7)
+  expected_counts = Map.put(expected_counts, 3, 7)
 
+  # 4
   defp filter_warning(warning = {:warn_matching, {'src/elixir_erl_compiler.erl', 59}, {:pattern_match, _lots_of_details}}),
-    do: {:ok, "return type not documented in erlang", warning, 4}
+    do: filtered(id: 4, comment: "return type not documented in erlang", data: warning)
 
-  expected = Map.put(expected, 4, 1)
+  expected_counts = Map.put(expected_counts, 4, 1)
 
+  # 5
   defp filter_warning(warning = {:warn_matching, {'lib/mix/tasks/compile.erlang.ex', 103}, {:pattern_match, ['pattern {\'error\', \'badarg\'}', _]}}),
-    do: {:ok, "return type not documented in erlang", warning, 5}
+    do: filtered(id: 5, comment: "return type not documented in erlang", data: warning)
 
-  expected = Map.put(expected, 5, 1)
+  expected_counts = Map.put(expected_counts, 5, 1)
 
+  # 6
   defp filter_warning(warning = {:warn_return_no_exit, {'lib/mix/tasks/test.ex', _}, {:no_return, [:only_normal, :raise_with_shell, 2]}}),
-    do: {:ok, "not annotated exception", warning, 6}
+    do: filtered(id: 6, comment: "not annotated exception", data: warning)
 
-  expected = Map.put(expected, 6, 1)
+  expected_counts = Map.put(expected_counts, 6, 1)
 
+  # 7
   defp filter_warning(warning = {:warn_return_no_exit, {'lib/mix/release.ex', _}, {:no_return, [:only_normal, :bad_umbrella!, 0]}}),
-    do: {:ok, "not annotated exception", warning, 7}
+    do: filtered(id: 7, comment: "not annotated exception", data: warning)
 
-  expected = Map.put(expected, 7, 1)
+  expected_counts = Map.put(expected_counts, 7, 1)
 
+  # 8
   defp filter_warning(warning = {:warn_return_no_exit, {'lib/mix/dep/loader.ex', _}, {:no_return, [:only_normal, :invalid_dep_format, 1]}}),
-    do: {:ok, "not annotated exception", warning, 8}
+    do: filtered(id: 8, comment: "not annotated exception", data: warning)
 
-  expected = Map.put(expected, 8, 1)
+  expected_counts = Map.put(expected_counts, 8, 1)
 
+  # 9
   defp filter_warning(warning = {:warn_return_no_exit, {'lib/iex.ex', _}, {:no_return, [:only_normal, :__break__!, 2]}}),
-    do: {:ok, "not annotated exception", warning, 9}
+    do: filtered(id: 9, comment: "not annotated exception", data: warning)
 
-  expected = Map.put(expected, 9, 1)
+  expected_counts = Map.put(expected_counts, 9, 1)
 
+  # 10
   defp filter_warning(warning = {:warn_return_no_exit, {'src/elixir_quote.erl', _}, {:no_return, [:only_normal, :bad_escape, 1]}}),
-    do: {:ok, "not annotated exception", warning, 10}
+    do: filtered(id: 10, comment: "not annotated exception", data: warning)
 
-  expected = Map.put(expected, 10, 1)
+  expected_counts = Map.put(expected_counts, 10, 1)
 
+  # 11
   defp filter_warning(warning = {:warn_return_no_exit, {'lib/mix/tasks/iex.ex', _}, {:no_return, [:only_normal, :run, 1]}}),
-    do: {:ok, "not annotated exception", warning, 11}
+    do: filtered(id: 11, comment: "not annotated exception", data: warning)
 
-  expected = Map.put(expected, 11, 1)
+  expected_counts = Map.put(expected_counts, 11, 1)
 
+  # 12
   defp filter_warning(warning = {:warn_return_no_exit, {'lib/iex/cli.ex', _}, {:no_return, [:only_normal]}}),
-    do: {:ok, "not annotated exit", warning, 12}
+    do: filtered(id: 12, comment: "not annotated exit", data: warning)
 
-  expected = Map.put(expected, 12, 1)
+  expected_counts = Map.put(expected_counts, 12, 1)
 
+  # 13
   defp filter_warning(warning = {:warn_return_no_exit, {'lib/gen_event.ex', _}, {:no_return, [:only_normal, :system_terminate, 4]}}),
-    do: {:ok, "not annotated exit", warning, 13}
+    do: filtered(id: 13, comment: "not annotated exit", data: warning)
 
-  expected = Map.put(expected, 13, 1)
+  expected_counts = Map.put(expected_counts, 13, 1)
 
+  # 14
   defp filter_warning(warning = {:warn_return_no_exit, {'lib/ex_unit/signal_handler.ex', 26}, {:no_return, [:only_normal, :handle_event, 2]}}),
-    do: {:ok, "not annotated exit", warning, 14}
+    do: filtered(id: 14, comment: "not annotated exit", data: warning)
 
-  expected = Map.put(expected, 14, 1)
+  expected_counts = Map.put(expected_counts, 14, 1)
 
+  # 15
   defp filter_warning(warning = {:warn_return_no_exit, {'lib/elixir/src/elixir_parser.yrl', _}, {:no_return, [:only_normal, function, _arity]}})
        when function in [
               :error_invalid_kw_identifier,
@@ -109,10 +124,11 @@ defmodule Dialyze do
               :yeccpars2_320_,
               :yeccpars2_86_
             ],
-       do: {:ok, "parser not annotated exception", warning, 15}
+       do: filtered(id: 15, comment: "parser not annotated exception", data: warning)
 
-  expected = Map.put(expected, 15, 14)
+  expected_counts = Map.put(expected_counts, 15, 14)
 
+  # 16
   defp filter_warning(warning = {:warn_return_no_exit, {'lib/elixir/src/elixir_parser.erl', _}, {:no_return, [:only_normal, function, _arity]}})
        when function in [
               :yeccpars2_357,
@@ -122,10 +138,11 @@ defmodule Dialyze do
               :yeccpars2_88,
               :yeccpars2_86
             ],
-       do: {:ok, "parser not annotated exception", warning, 16}
+       do: filtered(id: 16, comment: "parser not annotated exception", data: warning)
 
-  expected = Map.put(expected, 16, 6)
+  expected_counts = Map.put(expected_counts, 16, 6)
 
+  # 17
   defp filter_warning(warning = {:warn_unknown, {[], 0}, {:unknown_function, {module, :__impl__, 1}}})
        when module in [
               Collectable.Atom,
@@ -157,35 +174,41 @@ defmodule Dialyze do
               String.Chars.Reference,
               String.Chars.Tuple
             ],
-       do: {:ok, "some protocol consolidation stuff", warning, 17}
+       do: filtered(id: 17, comment: "some protocol consolidation stuff", data: warning)
 
-  expected = Map.put(expected, 17, 28)
+  expected_counts = Map.put(expected_counts, 17, 28)
 
+  # 18
   defp filter_warning(warning = {:warn_unknown, {[], 0}, {:unknown_function, {Hex, function, 0}}}) when function in [:start, :version],
-    do: {:ok, "Hex package loading gets handled by the Mix task", warning, 18}
+    do: filtered(id: 18, comment: "Hex package loading gets handled by the Mix task", data: warning)
 
-  expected = Map.put(expected, 18, 2)
+  expected_counts = Map.put(expected_counts, 18, 2)
 
+  # 19
   defp filter_warning(warning = {:warn_not_called, {'lib/base.ex', _}, {:unused_fun, [function, _]}}) when function in [:encode_pair_clauses, :shift, :encode_clauses, :decode_char_clauses, :decode_mixed_clauses, :decode_clauses, :bad_digit_clause],
-    do: {:ok, "functions inlined or only used to generate other functions at compile time", warning, 19}
+    do: filtered(id: 19, comment: "functions inlined or only used to generate other functions at compile time", data: warning)
 
-  expected = Map.put(expected, 19, 7)
+  expected_counts = Map.put(expected_counts, 19, 7)
 
+  # 20
   defp filter_warning(warning = {:warn_not_called, {'lib/system.ex', _}, {:unused_fun, [function, 1]}}) when function in [:read_stripped, :strip],
-    do: {:ok, "functions called only during elixir compilation time", warning, 20}
+    do: filtered(id: 20, comment: "functions called only during elixir compilation time", data: warning)
 
-  expected = Map.put(expected, 20, 2)
+  expected_counts = Map.put(expected_counts, 20, 2)
 
+  # 21
   defp filter_warning(warning = {:warn_matching, {'lib/mix/dep/loader.ex', line}, {:pattern_match_cov, ['variable _', '\'Elixir.Regex\'']}}) when line in [125, 155, 163],
-    do: {:ok, "redundant clause generated by is_struct macro for this literal atom", warning, 21}
+    do: filtered(id: 21, comment: "redundant clause generated by is_struct macro for this literal atom", data: warning)
 
-  expected = Map.put(expected, 21, 3)
+  expected_counts = Map.put(expected_counts, 21, 3)
 
+  # 22
   defp filter_warning(warning = {:warn_matching, {'lib/ex_unit/assertions.ex', 741}, {:pattern_match_cov, ['variable _', '\'Elixir.Regex\'']}}),
-    do: {:ok, "redundant clause generated by is_struct macro for this literal atom", warning, 22}
+    do: filtered(id: 22, comment: "redundant clause generated by is_struct macro for this literal atom", data: warning)
 
-  expected = Map.put(expected, 22, 1)
+  expected_counts = Map.put(expected_counts, 22, 1)
 
+  # 23
   defp filter_warning(
          warning =
            {:warn_matching, {'lib/dynamic_supervisor.ex', 350},
@@ -195,15 +218,17 @@ defmodule Dialyze do
                '{_,_,_,_,_,_} | \#{\'id\':=_, \'start\':={atom(),atom(),[any()]}, \'modules\'=>\'dynamic\' | [atom()], \'restart\'=>\'permanent\' | \'temporary\' | \'transient\', \'shutdown\'=>\'brutal_kill\' | \'infinity\' | non_neg_integer(), \'type\'=>\'supervisor\' | \'worker\'}'
              ]}}
        ),
-       do: {:ok, "overly defensive code", warning, 23}
+       do: filtered(id: 23, comment: "overly defensive code", data: warning)
 
-  expected = Map.put(expected, 23, 1)
+  expected_counts = Map.put(expected_counts, 23, 1)
 
+  # 24
   defp filter_warning(warning = {:warn_matching, {'lib/string_io.ex', 410}, {:guard_fail, [:is_list, '(_data@1::binary())']}}),
-    do: {:ok, "overly defensive code", warning, 24}
+    do: filtered(id: 24, comment: "overly defensive code", data: warning)
 
-  expected = Map.put(expected, 24, 1)
+  expected_counts = Map.put(expected_counts, 24, 1)
 
+  # 25
   defp filter_warning(
          warning =
            {:warn_matching, {'lib/mix/utils.ex', 694},
@@ -213,124 +238,125 @@ defmodule Dialyze do
                '\#{\'__struct__\':=\'Elixir.URI\', \'authority\':=\'nil\' | binary(), \'fragment\':=\'nil\' | binary(), \'host\':=\'nil\' | binary(), \'path\':=\'nil\' | binary(), \'port\':=\'nil\' | char(), \'query\':=\'nil\' | binary(), \'scheme\':=\'nil\' | binary(), \'userinfo\':=\'nil\' | binary()}'
              ]}}
        ),
-       do: {:ok, "overly defensive code", warning, 25}
+       do: filtered(id: 25, comment: "overly defensive code", data: warning)
 
-  expected = Map.put(expected, 25, 1)
+  expected_counts = Map.put(expected_counts, 25, 1)
 
+  # 26
   defp filter_warning(warning = {:warn_matching, {'lib/iex/helpers.ex', 604}, {:pattern_match, ['pattern <__key@1, \'nil\'>', '<<<_:64,_:_*8>>,<<_:80>> | string() | non_neg_integer()>']}}),
-    do: {:ok, "overly_defensive code", warning, 26}
+    do: filtered(id: 26, comment: "overly_defensive code", data: warning)
 
-  expected = Map.put(expected, 26, 1)
+  expected_counts = Map.put(expected_counts, 26, 1)
 
+  # 27
   defp filter_warning(warning = {:warn_matching, {'lib/mix/tasks/deps.compile.ex', 252}, {:guard_fail, ['_@6::\'true\'', '=:=', '\'nil\'']}}),
-    do: {:ok, "slightly dead code", warning, 27}
+    do: filtered(id: 27, comment: "slightly dead code", data: warning)
 
-  expected = Map.put(expected, 27, 1)
+  expected_counts = Map.put(expected_counts, 27, 1)
 
+  # 27
   defp filter_warning(
          warning =
            {:warn_matching, {'lib/calendar/time.ex', 642},
             {:pattern_match, ['pattern {\'error\', _reason@1}', '{\'ok\',\#{\'__struct__\':=\'Elixir.Time\', \'calendar\':=atom(), \'hour\':=non_neg_integer(), \'microsecond\':={non_neg_integer(),non_neg_integer()}, \'minute\':=non_neg_integer(), \'second\':=non_neg_integer()}}']}}
        ),
-       do: {:ok, "slightly dead code", warning, 28}
+       do: filtered(id: 28, comment: "slightly dead code", data: warning)
 
-  expected = Map.put(expected, 28, 1)
+  expected_counts = Map.put(expected_counts, 28, 1)
 
-  defp filter_warning(warning), do: {:warning, warning}
+  defp filter_warning(warning), do: unfiltered(data: warning)
 
-  @expected_counts expected
-  def expected_counts(), do: @expected_counts
-end
+  @expected_counts expected_counts
+  defp expected_counts(), do: @expected_counts
 
-results =
-  :dialyzer.run(
-    init_plt: 'plt-dir/.elixir.plt',
-    native: true,
-    warnings: [
-      :unknown
-    ],
-    files_rec: [
-      'elixir/lib/eex/ebin',
-      'elixir/lib/elixir/ebin',
-      'elixir/lib/ex_unit/ebin',
-      'elixir/lib/iex/ebin',
-      'elixir/lib/logger/ebin',
-      'elixir/lib/mix/ebin'
-    ]
-  )
-  |> Dialyze.filter()
-
-filtered =
-  results
-  |> Enum.reduce(%{}, fn
-    {:warning, _}, acc ->
-      acc
-
-    {:ok, message, warning, id}, acc ->
-      Map.update(
-        acc,
-        id,
-        %{count: 1, message: message, warnings: [warning]},
-        fn map ->
-          %{map | count: map.count + 1, warnings: [warning | map.warnings]}
-        end
+  def run() do
+    results =
+      :dialyzer.run(
+        init_plt: 'plt-dir/.elixir.plt',
+        warnings: [
+          :unknown
+        ],
+        files_rec: [
+          'elixir/lib/eex/ebin',
+          'elixir/lib/elixir/ebin',
+          'elixir/lib/ex_unit/ebin',
+          'elixir/lib/iex/ebin',
+          'elixir/lib/logger/ebin',
+          'elixir/lib/mix/ebin'
+        ]
       )
-  end)
+      |> filter()
 
-results =
-  (results ++
-     (Dialyze.expected_counts()
+    actual_counts =
+      results
+      |> Enum.reduce(%{}, fn
+        filtered(id: id), actual_counts ->
+          Map.update(actual_counts, id, 1, &(&1 + 1))
+
+        _, actual_counts ->
+          actual_counts
+      end)
+
+    unexpected_counts_warnings =
+      expected_counts()
       |> Enum.filter(fn
         {id, expected_count} ->
-          filtered[id][:count] != expected_count
+          actual_counts[id] != expected_count
       end)
       |> Enum.map(fn
         {id, expected_count} ->
-          {:unexpected_count, filtered[id][:message], filtered[id][:warnings], id, filtered[id][:count], expected_count}
-      end)))
-  |> Enum.sort_by(fn
-    {:warning, _} -> 1
-    {:unexpected_count, _, _, _, _, _} -> 2
-    {:ok, _, _, _} -> 3
-  end)
+          unexpected_count(id: id, actual: actual_counts[id], expected: expected_count)
+      end)
 
-has_potential_issues? =
-  case results do
-    [{:warning, _} | _] -> true
-    [{:unexpected_count, _, _, _, _, _} | _] -> true
-    _ -> false
+    results =
+      (unexpected_counts_warnings ++ results)
+      |> Enum.sort_by(fn
+        unfiltered() -> 1
+        unexpected_count() -> 2
+        filtered() -> 3
+      end)
+
+    has_potential_issues? =
+      case results do
+        [unfiltered() | _] -> true
+        [unexpected_count() | _] -> true
+        _ -> false
+      end
+
+    results
+    |> Stream.map(fn
+      unfiltered(data: warning) ->
+        formatted_warning =
+          warning
+          |> :dialyzer.format_warning(indent_opt: true, filename_opt: :fullpath)
+          |> to_string()
+
+        "############################################################\n" <>
+          "Potential issue: \n#{formatted_warning}\n\n" <>
+          "data: #{inspect(warning)}\n\n"
+
+      unexpected_count(id: id, actual: actual_count, expected: expected_count) ->
+        "############################################################\n" <>
+          "Expected count of filtered non-issues ##{id} is off! Expected #{expected_count}, found #{actual_count || 0} issues\n\n"
+
+      filtered(id: id, comment: comment, data: warning) ->
+        formatted_warning =
+          warning
+          |> :dialyzer.format_warning(indent_opt: true, filename_opt: :fullpath)
+          |> to_string()
+
+        "############################################################\n" <>
+          "Filtered non-issue ##{id} (#{comment}):\n#{formatted_warning}\n\n" <>
+          "data: #{inspect(warning)}\n\n"
+    end)
+    |> Stream.into(File.stream!("report", [:write, :utf8]))
+    |> Stream.into(IO.stream(:stdio, :line))
+    |> Stream.run()
+
+    if(has_potential_issues?) do
+      System.halt(1)
+    end
   end
-
-results
-|> Stream.map(fn
-  {:warning, warning} ->
-    formatted_warning =
-      warning
-      |> :dialyzer.format_warning(indent_opt: true, filename_opt: :fullpath)
-      |> to_string()
-
-    "############################################################\n" <>
-      "Potential issue: \n#{formatted_warning}\n\n" <>
-      "raw form: #{inspect(warning)}\n\n"
-
-  {:unexpected_count, _message, _warnings, id, count, expected_count} ->
-    "############################################################\n" <>
-      "Expected count of filtered non-issues ##{id} is off! Expected #{expected_count}, found #{count || 0} issues\n\n"
-
-  {:ok, message, warning, id} ->
-    formatted_warning =
-      warning
-      |> :dialyzer.format_warning(indent_opt: true, filename_opt: :fullpath)
-      |> to_string()
-
-    "############################################################\n" <>
-      "Filtered non-issue ##{id} (#{message}):\n#{formatted_warning}\n\n" <>
-      "raw form: #{inspect(warning)}\n\n"
-end)
-|> Stream.into(File.stream!("report", [:write, :utf8]))
-|> Stream.into(IO.stream(:stdio, :line))
-|> Stream.run()
-
-if(has_potential_issues?) do
-  System.halt(1)
 end
+
+Dialyze.run()
