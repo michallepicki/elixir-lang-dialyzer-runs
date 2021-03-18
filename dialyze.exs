582f077af6763bb9342c8cac594ff464070b705a
Michał Łępicki <michallepicki@users.noreply.github.com>
2021-03-18 07:41:45 +0100

    Update dialyze.exs

--- dialyze.exs
+++ dialyze.exs
@@ -123,7 +123,7 @@ defmodule Dialyze do
 
   expected_counts = Map.put(expected_counts, 22, 1)
 
-  defp filter_warning(warning = {:warn_matching, {'lib/mix/tasks/deps.compile.ex', 252}, {:guard_fail, ['_@6::\'true\'', '=:=', '\'nil\'']}}),
+  defp filter_warning(warning = {:warn_matching, {'lib/mix/tasks/deps.compile.ex', 259}, {:guard_fail, ['_@6::\'true\'', '=:=', '\'nil\'']}}),
     do: filtered(id: 23, comment: "slightly dead code", data: warning)
 
   expected_counts = Map.put(expected_counts, 23, 1)
