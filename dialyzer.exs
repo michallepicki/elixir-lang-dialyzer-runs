79d6b66f58ee4a9f667f6de2610b974d6837beca
Michał Łępicki <michallepicki@users.noreply.github.com>
2021-12-03 08:10:22 +0100

    Update dialyzer.exs

--- dialyzer.exs
+++ dialyzer.exs
@@ -85,7 +85,7 @@ defmodule Dialyzer do
   @counts 1
   expected_counts = Map.put(expected_counts, @id, @counts)
 
-  defp filter(expected = {:warn_matching, {'lib/mix/utils.ex', 755}, {:pattern_match, ['pattern \'nil\'', '\#{\'__struct__\':=\'Elixir.URI\', \'authority\':=\'Elixir.URI\':authority(), \'fragment\':=\'nil\' | binary(), \'host\':=\'nil\' | binary(), \'path\':=binary(), \'port\':=\'nil\' | char(), \'query\':=\'nil\' | binary(), \'scheme\':=\'nil\' | binary(), \'userinfo\':=\'nil\' | binary()}']}}),
+  defp filter(expected = {:warn_matching, {'lib/mix/utils.ex', 755}, {:pattern_match, ['pattern \'nil\'', '\#{\'__struct__\':=\'Elixir.URI\', \'authority\':=\'Elixir.URI\':authority(), \'fragment\':=\'nil\' | binary(), \'host\':=\'nil\' | binary(), \'path\':=\'nil\' | binary(), \'port\':=\'nil\' | char(), \'query\':=\'nil\' | binary(), \'scheme\':=\'nil\' | binary(), \'userinfo\':=\'nil\' | binary()}']}}),
     do: filtered(comment: "overly defensive code", id: @id, data: expected)
 
   id = id + 1
