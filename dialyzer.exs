605ecb3ab22ddb81f1cf6787252a80347553dbb6
Michał Łępicki <michallepicki@users.noreply.github.com>
2022-08-31 08:51:31 +0200

    Update dialyzer.exs

--- dialyzer.exs
+++ dialyzer.exs
@@ -334,7 +334,7 @@ defmodule Dialyzer do
            {:warn_matching, {'lib/kernel.ex', line},
             {:pattern_match, ['pattern \'false\'', '\'true\'']}}
        )
-       when line in [1950, 3455, 3849, 3919, 4304],
+       when line in [1950, 3455, 3850, 3920, 4305],
        do: filtered(comment: "inlined bootstrap check stuff", id: @id, data: expected)
 
   @id 150
