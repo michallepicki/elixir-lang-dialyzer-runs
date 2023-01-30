fe5f2359b540f1071cc08db6e0d92477db18a33e
Michał Łępicki <michallepicki@users.noreply.github.com>
2023-01-30 08:50:14 +0100

    update line number

--- dialyzer.exs
+++ dialyzer.exs
@@ -251,7 +251,7 @@ defmodule Dialyzer do
 
   defp filter(
          expected =
-           {:warn_matching, {'lib/mix/utils.ex', 760},
+           {:warn_matching, {'lib/mix/utils.ex', 763},
             {:pattern_match,
              [
                'pattern \'nil\'',
