19b53228fcf4ed6eef82bd3c4b1d61562b963ab9
Michał Łępicki <michallepicki@users.noreply.github.com>
2023-03-25 13:34:17 +0100

    prep for 26

--- dialyzer.exs
+++ dialyzer.exs
@@ -225,6 +225,19 @@ defmodule Dialyzer do
 
 
   @id __ENV__.line
+  expected_counts =
+    if System.otp_release() > "26",
+      do: Map.put(expected_counts, @id, 1),
+      else: Map.put(expected_counts, @id, 0)
+
+  defp filter(
+        expected =
+          {:warn_unknown, {~c"lib/iex/cli.ex", _}, {:unknown_function, {:user, :start, 0}}}
+      ),
+      do: filtered(comment: "function used only conditionally on otp < 26", id: @id, data: expected)
+
+
+  @id __ENV__.line
   expected_counts = Map.put(expected_counts, @id, 2)
 
   defp filter(
@@ -282,7 +295,10 @@ defmodule Dialyzer do
        do: filtered(comment: "overly_defensive code", id: @id, data: expected)
 
   @id __ENV__.line
-  expected_counts = Map.put(expected_counts, @id, 22)
+  expected_counts =
+    if System.otp_release() < "26",
+      do: Map.put(expected_counts, @id, 22),
+      else: Map.put(expected_counts, @id, 0)
 
   defp filter(expected = {:warn_unknown, {file, 1}, {:unknown_function, {module, :__impl__, 1}}})
        when module in [
@@ -312,7 +328,10 @@ defmodule Dialyzer do
        do: filtered(comment: "some protocol consolidation stuff", id: @id, data: expected)
 
   @id __ENV__.line
-  expected_counts = Map.put(expected_counts, @id, 6)
+  expected_counts =
+    if System.otp_release() < "26",
+      do: Map.put(expected_counts, @id, 6),
+      else: Map.put(expected_counts, @id, 0)
 
   defp filter(
          expected =
