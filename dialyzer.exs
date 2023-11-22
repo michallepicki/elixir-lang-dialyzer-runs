a7dcabde6e69d7f7179809fbb05a4687f73f51b7
Michał Łępicki <michallepicki@users.noreply.github.com>
2023-11-22 10:56:37 +0100

    Update dialyzer.exs

--- dialyzer.exs
+++ dialyzer.exs
@@ -354,7 +354,7 @@ defmodule Dialyzer do
            {:warn_matching, {~c"lib/kernel.ex", line},
             {:pattern_match, [~c"pattern \'false\'", ~c"\'true\'"]}}
        )
-       when line in [2060, 3560, 3953, 4050, 4437],
+       when line in [2060, 3560, 3955, 4052, 4439],
        do: filtered(comment: "inlined bootstrap check stuff", id: @id, data: dialyzer_warning)
 
   @id __ENV__.line
