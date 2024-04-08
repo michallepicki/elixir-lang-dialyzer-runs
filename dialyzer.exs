a0ba28403ce02f1bed903315f6e86e6586b876b8
Michał Łępicki <michallepicki@users.noreply.github.com>
2024-04-08 08:51:51 +0200

    Update dialyzer.exs

--- dialyzer.exs
+++ dialyzer.exs
@@ -200,7 +200,7 @@ defmodule Dialyzer do
   # discussed in https://github.com/elixir-lang/elixir/issues/11092
   defp filter(
          dialyzer_warning =
-           {:warn_matching, {~c"lib/calendar/time.ex", 758},
+           {:warn_matching, {~c"lib/calendar/time.ex", 802},
             {:pattern_match,
              [
                ~c"pattern {'error', _reason@1}",
