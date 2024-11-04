2b3c2dfd83e5a3ad70428e8b99790983b29e5199
Michał Łępicki <michallepicki@users.noreply.github.com>
2024-11-04 08:41:52 +0100

    Update dialyzer.exs

--- dialyzer.exs
+++ dialyzer.exs
@@ -181,7 +181,7 @@ defmodule Dialyzer do
   # may be resolved in https://github.com/elixir-lang/elixir/issues/9465
   defp filter(
          dialyzer_warning =
-           {:warn_failing_call, {~c"lib/logger.ex", {931, 50}},
+           {:warn_failing_call, {~c"lib/logger.ex", {932, 41}},
             {:call, [Logger, :__do_log__, _, [3], :only_sig, _, _, {false, :none}]}}
        ),
        do:
