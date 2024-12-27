8e2d78c3f3a37f37ab83c5bbbcdc16538cf6603a
Michał Łępicki <michallepicki@users.noreply.github.com>
2024-12-28 00:13:19 +0100

    ignore warning

--- dialyzer.exs
+++ dialyzer.exs
@@ -316,6 +316,15 @@ defmodule Dialyzer do
        ),
        do: filtered(comment: "overly_defensive code", id: @id, data: dialyzer_warning)
 
+  @id __ENC__.line
+  expected_counts = Map.put(expected_counts, @id, 1)
+
+  defp filter(
+         dialyzer_warning =
+           {:warn_matching, {~c"src/elixir_erl.erl", {515, 1}}, {:pattern_match, [~c"pattern <[], Opts>", ~c"<[any(),...],[any(),...]>"]}}
+       ),
+       do: filtered(comment: "currently redundant clause because at least the ExCk chunk will be present", id: @id, data: dialyzer_warning)
+
   @id __ENV__.line
   expected_counts = Map.put(expected_counts, @id, 5)
 
